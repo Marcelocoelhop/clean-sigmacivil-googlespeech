35125c6e15f85998c22c7fb465e55379794e189f
Marcelocoelhop <marcelomcoelho@sigmacivil.com.br>
2023-10-03 18:09:24 -0300

    feat: ensure ZodValidatorAdapter throws if is not a string

---
+++ tests/infra/validators/zod-validator-adapter.spec.ts
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker'
+import { type ZodObject, z } from 'zod'
+
+class ZodValidatorAdapter {
+  private schema: ZodObject<any, 'strip', z.ZodTypeAny, any, any>
+  constructor(private readonly fieldName: string) {
+    this.schema = z.object({})
+  }
+
+  string(): ZodObject<any, 'strip', z.ZodTypeAny, any, any> {
+    this.schema = this.schema.extend({
+      [this.fieldName]: z.string(),
+    })
+    return this.schema
+  }
+}
+
+describe('ZodValidatorAdapter', () => {
+  it('should throw if is not a string', () => {
+    const fieldName = faker.lorem.word()
+    const sut = new ZodValidatorAdapter(fieldName)
+
+    const schema = sut.string()
+
+    expect(() => {
+      schema.parse({ [fieldName]: faker.number.int(1) })
+    }).toThrow()
+  })
+})
