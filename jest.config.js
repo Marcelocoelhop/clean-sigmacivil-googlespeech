b6fcd5370a99ca921e96b749e61eb933ed2cb10d
Marcelocoelhop <marcelomcoelho@sigmacivil.com.br>
2023-10-04 17:44:59 -0300

    feat: configure project build

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@ module.exports = {
   roots: ['<rootDir>'],
   moduleFileExtensions: ['js', 'ts'],
   testPathIgnorePatterns: ['./node_modules/'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/index.ts',
+    '!src/**/domain/**',
+    '!src/**/contracts/**',
+    '!src/**/main/adapters/**',
+    '!src/**/main/server.ts',
+  ],
   moduleNameMapper: {
     '^@/tests/(.*)$': '<rootDir>/tests/$1',
     '^@/(.*)$': '<rootDir>/src/$1'
